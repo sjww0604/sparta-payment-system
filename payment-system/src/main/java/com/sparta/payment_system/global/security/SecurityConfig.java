44881f6e7efd52073f4e71202c5b56c9fed7ce5b
LeoOH5 <bbin0409@naver.com>
2025-11-20 10:13:19 +0900

    fix : SecurityConfig 위치 변경

--- payment-system/src/main/java/com/sparta/payment_system/global/security/SecurityConfig.java
+++ payment-system/src/main/java/com/sparta/payment_system/global/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.sparta.payment_system.config;
+package com.sparta.payment_system.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
