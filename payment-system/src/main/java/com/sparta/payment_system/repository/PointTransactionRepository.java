55d91bb3c358e3b7961e63dcf78f80cc14991b9d
sjww0306 <sjww03@gmail.com>
2025-11-21 10:14:03 +0900

    feat : 후처리 기능 중 취소건에 대한 포인트 롤백 기능 구현
    - 특정 주문건에 대한 처리과정에서 있었던 모든 적립내역을 netPoints로 설정
    
    - 기존 PointTransaction을 활용하여 회수처리 진행하도록 로직 구현

--- payment-system/src/main/java/com/sparta/payment_system/repository/PointTransactionRepository.java
+++ payment-system/src/main/java/com/sparta/payment_system/repository/PointTransactionRepository.java
@@ -1,11 +1,16 @@
 package com.sparta.payment_system.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.sparta.payment_system.entity.Order;
 import com.sparta.payment_system.entity.PointTransaction;
+import com.sparta.payment_system.entity.User;
 
 @Repository
 public interface PointTransactionRepository extends JpaRepository<PointTransaction, Long> {
 
+	List<PointTransaction> findByUserAndOrder(User user, Order order);
 }
--- payment-system/src/main/java/com/sparta/payment_system/service/PointService.java
+++ payment-system/src/main/java/com/sparta/payment_system/service/PointService.java
@@ -3,6 +3,7 @@ package com.sparta.payment_system.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -74,5 +75,42 @@ public class PointService {
 		);
 
 		pointTransactionRepository.save(pointTransaction);
+	}
+
+	/* 결제 전액 취소 시 포인트 회수
+	 * - 해당 주문과 관련된 포인트 지급,회수에 대한 과정을 netPoint로 계산한 뒤
+	 * netPoints가 양수인 경우 그만큼 음수 포인트를 추가로 기록하여 회수처리를 진행함 (향후 부분환불 로직 고려한 개념)
+	 * - 이미 한 번 이상 회수된 주문에 대해서는 중복 회수되지 않도록 기준 설정
+	 */
+	public void rollbackPointsAfterCancel(Order order) {
+		if (order == null) {
+			return;
+		}
+
+		User user = order.getUser();
+		if (user == null) {
+			return;
+		}
+
+		// 취소 요청 주문과 관련된 모든 포인트 트랜잭션 조회
+		List<PointTransaction> transactions =
+			pointTransactionRepository.findByUserAndOrder(user, order);
+		if (transactions == null || transactions.isEmpty()) {
+			return; // 이력 없으면 회수할 데이터 없음
+		}
+
+		int netPoints = transactions.stream()
+			.mapToInt(PointTransaction::getPoints)
+			.sum();
+
+		if (netPoints > 0) {
+			PointTransaction rollbackTransaction = new PointTransaction(
+				user,
+				order,
+				-netPoints,
+				null
+			);
+			pointTransactionRepository.save(rollbackTransaction);
+		}
 	}
 }
