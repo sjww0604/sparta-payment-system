685e2617ea05f92fd8501d7637bb62cc4adb2880
Juwoojae <juwqq1234@naver.com>
2025-11-25 19:24:08 +0900

    refactor: 스프링 시큐리티 컨피그 적용 - 인가된 클라이언트 요청에 대해서만 허용하는 검증 적용

--- payment-system/src/main/java/com/sparta/payment_system/global/security/SecurityConfig.java
+++ payment-system/src/main/java/com/sparta/payment_system/global/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**","/**").permitAll()
+                        .requestMatchers("/css/**", "/js/**","/", "/api/auth/**","/api/payment").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
